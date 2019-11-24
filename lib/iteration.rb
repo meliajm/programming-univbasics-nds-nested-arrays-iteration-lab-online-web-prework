ef1fdcf07b2b3fe64ca5475a399b0311d396350e
melia miller <meliajm@gmail.com>
2019-11-24 22:55:29 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,20 +6,42 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  row_index = 0
+  new_array = []
+  while row_index < src.count do
+    # puts "I love " + src[row_index][0] + " and " + src[row_index][1] + " on my pizza"
+    new_array.push("I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza")
+    row_index += 1
+  end
+  new_array
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  row_index = 0 
+  new_array = []
+  while row_index < src.count do 
+    src[row_index] = src[row_index].sort 
+    new_array.push(src[row_index][-1])
+    row_index += 1
+  end
+  new_array
 end
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
+  # if both numbers in the pair are even, then add both those numbers to the total
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  # the number was even. Review the operator documentation if you've forgotten this!
+  new_array = []
+  row_index = 0 
+  while row_index < src.count do 
+    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
+      new_array.push(src[row_index][0]+src[row_index][1])
+    end
+    row_index += 1
+  end
+  new_array.sum
 end

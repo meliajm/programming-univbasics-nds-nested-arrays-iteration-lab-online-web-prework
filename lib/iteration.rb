264fc164cf0221e20d42bce6c5315da251cb21dd
melia miller <meliajm@gmail.com>
2019-11-24 22:48:57 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -40,8 +40,8 @@ def total_even_pairs(src)
   while row_index < src.count do 
     if src[row_index][0] && src[row_index][1] % 2 == 0
       new_array.push(src[row_index][0]+src[row_index][1])
+    row_index += 1
     end
-    row_index += 1 
   end
   new_array
 end
